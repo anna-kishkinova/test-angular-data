8fa9ad48a73c07350501adc1e305993b1d4cef89
Hanna_Kishkinova@epam.com <Hanna_kishkinova@epam.com>
2021-12-06 18:33:46 +0200

    123f

--- data-json.ts
+++
@@ -1,1 +0,0 @@
-'[{"id":1,"animal":"cat","name":"Alex","age":3},{"id":2,"animal":"dog","name":"Rex","age":5},{"id":3,"animal":"dog","name":"Bax","age":1.7},{"id":4,"animal":"cat","name":"Simon","age":1.1}]'
